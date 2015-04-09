2d42aa08e0921535094b5c0a64f000aa85e1f0a4
qizhipeng <572918136@qq.com>
2015-04-09 17:27:21 +0800

    通风一班戚志鹏的作业

---
+++ test1-1.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+print'戚志鹏'
+import math
+f1=(math.exp(3*0.1)-math.exp(3))/0.1
+print f1
+f2=(math.exp(3*0.01)-math.exp(3))/0.01
+print f2
+f3=(math.exp(3*0.001)-math.exp(3))/0.001
+print f3
+f4=(math.exp(3*0.0001)-math.exp(3))/0.0001
+print f4
+g4=math.exp(3)
+print g4
+
+print '近似导数：',f4
+print '精确导数值',g4
