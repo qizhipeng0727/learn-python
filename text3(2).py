badebfb4da797824ebcf269cd31edf314df67d7e
qizhipeng <572918136@qq.com>
2015-04-14 11:02:40 +0800

    4月14日   物理编程作业 第二份

---
+++ text3(2).py
@@ -0,0 +1,16 @@
+#-*- coding:utf-8 -*-
+#姓名：戚志鹏
+#1403050115
+#通风一班
+'''
+求物理公式
+'''
+
+import math
+R,m,n,NA,M,T,k,p=8.31,5.31e-26,2.45e25,6.022e23,32e-3,300.15,1.38e-23,1.013e5
+print 'n=p/(k*T)=',n
+print 'n=M/NA=',n
+print 'rho=n*m=',rho
+print 'v=math.sqrt(3*R*T/M)=',math.sqrt(v**2)
+print 'epsilonk=3/2*k*T=',epsilonk
+print 'epsilon=m/M*5/2*R*T=',epsilon
