82a444d5b3d61a1600b89969fd86793029541dfc
qizhipeng <572918136@qq.com>
2015-04-16 23:10:18 +0800

    戚志鹏第二次作业修改后

--- text3.py
+++ text3.py
@@ -6,10 +6,20 @@
 求物理公式
 '''
 
+import math
+k,p,T=1.38e-23,1.013e5,300.15
+n=p/(k*T)
+print 'n=',n
+M,NA=32e-3,6.022e23
+n=M/NA
+print 'n=',n
+n,m=2.45e25,5.31e-26
+rho=n*m
+print 'rho=',rho
 R=8.31
-T1=273.15
-T2=773.15
-a=(2.5)*R*T1
-b=(2.5)*R*T2
-
-print a,b
+v=math.sqrt(3*R*T/M)
+print 'math.sqrt(v**2)=',math.sqrt(v**2)
+epsilonk=3/2*k*T
+print 'epsilonk=',epsilonk
+epsilon=m/M*5/2*R*T
+print 'epsilon=',epsilon
